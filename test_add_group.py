576b2a870e29a0acb2981e8bfafe7fa16bfbfce4
Schiwa-ru <141829350+Schiwa-ru@users.noreply.github.com>
2023-11-21 10:30:10 +0300

    добавление контакта финал без фото

--- test_add_group.py
+++ test_add_group.py
@@ -12,30 +12,22 @@ class UntitledTestCase(unittest.TestCase):
     
     def test_add_group(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.open_groups(wd)
         self.create_group(wd, Group (name="name", logo="logo", comment="comment"))
-        self.return_group_page(wd)
         self.logout(wd)
 
     def test_empty_group(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.open_groups(wd)
         self.create_group(wd, Group (name="", logo="", comment=""))
-        self.return_group_page(wd)
         self.logout(wd)
 
     def logout(self, wd):
         wd.find_element_by_link_text("Logout").click()
 
-    def return_group_page(self, wd):
-        wd.find_element_by_link_text("group page").click()
-
     def create_group(self, wd, group):
         #create new group
+        wd.find_element_by_link_text("groups").click()
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
@@ -47,12 +39,12 @@ class UntitledTestCase(unittest.TestCase):
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.comment)
         wd.find_element_by_name("submit").click()
+        wd.find_element_by_link_text("group page").click()
 
-    def open_groups(self, wd):
-        wd.find_element_by_link_text("groups").click()
 
     def login(self, wd, username, password):
         #login
+        wd.get("http://localhost/addressbook/")
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(username)
@@ -61,9 +53,6 @@ class UntitledTestCase(unittest.TestCase):
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//input[@value='Login']").click()
-
-    def open_home_page(self, wd):
-        wd.get("http://localhost/addressbook/")
 
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)
